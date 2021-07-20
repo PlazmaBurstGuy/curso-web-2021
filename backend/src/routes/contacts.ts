70706b3c4aff51f15c28219325f857fd9436e793
PlazmaBurstGuy <pedrosonic@icloud.com>
2021-07-20 20:07:48 -0300

    crud basico

---
+++ backend/src/routes/contacts.ts
@@ -0,0 +1,42 @@
+import {Router, Request, Response} from 'express'
+import { Contact } from '../model/ContactModel'
+
+export class ContactRouter{
+
+    contacts: Contact[] = [
+        {
+            name: "pedro",
+            phone: "1234567"
+        }
+    ]
+    
+    public getRoutes(): Router {
+
+     const router: Router = Router()
+
+     router.get('/', (req: Request, res: Response) => {
+         res.send(this.contacts)
+     })
+
+     router.post('/', (req: Request, res: Response) => {
+         const contact = req.body
+         this.contacts.push(contact)
+         res.send('Hello World')
+     })
+
+     router.put('/', (req: Request, res: Response) => {
+        res.send('Hello World')
+    })
+
+    router.delete('/:id', (req: Request, res: Response) => {
+        const id: number = Number(req.params.id)
+        this.contacts.splice(id, 1)
+
+
+        res.send(this.contacts)
+    })
+
+
+     return router
+    }
+}
